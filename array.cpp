612d69f1fda2f0beee8ba47b2ce99aab6e2f8e83
rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2022-01-04 23:24:08 +0600

    array traversing

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    // array traversing
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+
+
+    cout<<endl;
+    return 0;
+}
